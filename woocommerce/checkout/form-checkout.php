e4a399085a108770ac9d69d2b695773119016861
myehrajat <31361589+myehrajat@users.noreply.github.com>
2018-12-04 13:01:55 +0330

    fix  outdated wc template error
    file has been replaced by wc original template

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -2,31 +2,36 @@
 /**
  * Checkout Form
  *
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     2.3.0
+ * This template can be overridden by copying it to yourtheme/woocommerce/checkout/form-checkout.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce/Templates
+ * @version 3.5.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-wc_print_notices();
-
 do_action( 'woocommerce_before_checkout_form', $checkout );
 
-// If checkout registration is disabled and not logged in, the user cannot checkout
-if ( ! $checkout->enable_signup && ! $checkout->enable_guest_checkout && ! is_user_logged_in() ) {
-	echo apply_filters( 'woocommerce_checkout_must_be_logged_in_message', esc_html__( 'You must be logged in to checkout.', "rentit" ) );
+// If checkout registration is disabled and not logged in, the user cannot checkout.
+if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
+	echo esc_html( apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) ) );
 	return;
 }
 
-// filter hook for include new pages inside the payment method
- ?>
+?>
+
 <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
 
-
-<?php if ( sizeof( $checkout->checkout_fields ) > 0 ) : ?>
+	<?php if ( $checkout->get_checkout_fields() ) : ?>
 
 		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
@@ -42,11 +47,9 @@ if ( ! $checkout->enable_signup && ! $checkout->enable_guest_checkout && ! is_us
 
 		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 
-			<h3 id="order_review_heading"  class="block-title alt"><i class="fa fa-angle-down"></i>
-				<?php  esc_html_e( 'Your order', "rentit" ); ?></h3>
-
-
 	<?php endif; ?>
+
+	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
 
 	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
--- woocommerce/notices/success.php
+++ woocommerce/notices/success.php
@@ -2,30 +2,33 @@
 /**
  * Show messages
  *
- * @author        WooThemes
- * @package    WooCommerce/Templates
- * @version     3.3.0
+ * This template can be overridden by copying it to yourtheme/woocommerce/notices/success.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see         https://docs.woocommerce.com/document/template-structure/
+ * @package     WooCommerce/Templates
+ * @version     3.5.0
  */
 
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-if (!$messages) {
-    return;
+if ( ! $messages ) {
+	return;
 }
 
-if (get_option('woocommerce_cart_redirect_after_add') == 'yes' && $_SERVER['REQUEST_URI'] == '/checkout/') {
-
-} else {
-    ?>
-
-    <?php foreach ($messages as $message) : ?>
-
-        <div class="alert alert-success fade in">
-            <button class="close" data-dismiss="alert" type="button">×</button>
-            <?php echo wp_kses_post($message); ?>
-        </div>
-    <?php endforeach;
-}
 ?>
+
+<?php foreach ( $messages as $message ) : ?>
+	<div class="woocommerce-message" role="alert">
+		<?php
+			echo wc_kses_notice( $message );
+		?>
+	</div>
+<?php endforeach; ?>
